ceb202ebe459071a70037095e6dad641d2e5b362
LuChao <atorber@163.com>
2024-11-03 23:24:38 +0800

    ✨ feat: 增强事件监听器的日志记录，更新类型导入方式

--- src/agents/Devo919-Gewechat/client/mod.ts
+++ src/agents/Devo919-Gewechat/client/mod.ts
@@ -1,18 +1,19 @@
 import { Client, xml2js } from './module/index.js'
-import { Options } from './types/types.js'
 import { log, logger } from './utils/logger.js'
-import * as GeweTypes from './types/types.js'
-import * as MessageTypes from './types/message.js'
-import * as NoticeTypes from './types/notice.js'
-import * as RoomTypes from './types/room.js'
+import type * as MessageTypes from './types/message.js'
+import type * as NoticeTypes from './types/notice.js'
+import type * as RoomTypes from './types/room.js'
+import type * as GeweTypes from './types/types.js'
 
 export {
   Client,
   log,
   logger,
   xml2js,
+}
+export type {
   MessageTypes,
   NoticeTypes,
   RoomTypes,
+  GeweTypes,
 }
-export type { Options, GeweTypes }
